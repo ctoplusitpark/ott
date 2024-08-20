b830d3ccc0a107e7a9aa67006a88f609c7907009
Namitha-234 <namitha23498@gmail.com>
2024-08-20 13:23:12 +0530

    initial commit

--- client/src/Signin.jsx
+++ client/src/Signin.jsx
@@ -10,29 +10,8 @@ import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 function Signin() {
-  const [email,setEmail] = useState('')
-  const [password,setPassword] = useState('')
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post('http://localhost:5000/https://api.indrajala.in/api/user/login', {
-        email,
-        password,
-      });
-
-      if (response.status === 200) {
-        setMessage('Login successful!');
-      }
-    } catch (error) {
-      if (error.response && error.response.status === 401) {
-        setMessage('Invalid credentials');
-      } else {
-        setMessage('Something went wrong');
-      }
-    }
-}
+   const [email,setEmail] = useState('')
+   const [password,setPassword] = useState('')
 
   return (
     <div className="d-flex justify-content-center align-items-center bg-black vh-100">
@@ -94,6 +73,6 @@ function Signin() {
       </div>
     </div>
   );
-}
 
+}
 export default Signin;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const jwt = require('jsonwebtoken')
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const jwt = require('jsonwebtoken')
 
-const app = express();
-app.use(bodyParser.json());
+// const app = express();
+// app.use(bodyParser.json());
 
-app.use(cors());
-app.use(bodyParser.json());
-app.post('/https://api.indrajala.in/api/user/login', (req, res) => {
-  const { email, password } = req.body;
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.post('/https://api.indrajala.in/api/user/login', (req, res) => {
+//   const { email, password } = req.body;
 
-  if (email === 'john.doe@example.com' && password === 'StrongPassword123') {
-    res.status(200).json({ message: 'Login successful!' });
-  } else {
-    res.status(401).json({ message: 'Invalid credentials' });
-  }
-});
+//   if (email === 'john.doe@example.com' && password === 'StrongPassword123') {
+//     res.status(200).json({ message: 'Login successful!' });
+//   } else {
+//     res.status(401).json({ message: 'Invalid credentials' });
+//   }
+// });
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+// app.listen(5000, () => console.log('Server running on http://localhost:5000'));
